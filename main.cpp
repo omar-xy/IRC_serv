e7bb704ea51a4b06f7275a6343a06818411cfa89
Marouan Doulahiane <mdoulahi@e1r6p1.1337.ma>
2024-02-17 08:12:50 +0100

    by cypher

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int ac, char **av)
 		std::cerr << "Error: usage ./ircserv <port> <password>" << std::endl;
 		exit(1);
 	}
-	Server server = Server(AF_INET, 5000, INADDR_ANY, av[2]);
+	Server server = Server(AF_INET, 5000, INADDR_ANY, av[2]); // lport hiya 5000 
 
-	std::cout << "the end" << std::endl;
+	std::cout << "the end!" << std::endl;
 }

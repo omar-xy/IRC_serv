a0cc4972b8d2523d6c2c7e5f79f113e9bde44b2c
cypher <marouandoulahiane@gmail.com>
2024-04-29 02:55:20 +0100

    start creating a bot as a client

--- bonus/bot.cpp
+++ bonus/bot.cpp
@@ -1,67 +1,21 @@
-#include "../headers/IRCserv.hpp"
-#include "../headers/replies.hpp"
-#include <iostream>
-#include <string>
-#include <ctime>
-#include <vector>
-
-void helpCommand(Client &client) 
+#include "Bot.hpp"
+#include <stdexcept>
+#include <sstream>
+Bot::Bot(int ip, int port, std::string pass)
 {
-    client.send_message("/help - Displays this help message.\n");
-    client.send_message("/time - Displays the current time.\n");
-    client.send_message("/channels - Lists the channels the bot has joined.\n");
-    client.send_message("/funFact - Displays a random fact.\n");
-}
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0)
+        throw std::runtime_error(":socket() fail!");
+    sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(sockaddr_in));
+    serverAddress.sin_family = AF_INET;
+    std::stringstream in_port(pass);
+    if (!(in_port >> port) || this->port < 1024 || this->port > 65535)
+        throw std::runtime_error("non valid port!");
 
-
-void timeCommand(Client &client) {
-    time_t now = time(0);
-    char* dt = ctime(&now);
-    std::string ti(dt);
-    client.send_message("The current date and time is: " + ti + "\n");
-}
-
-
-void channelsCommand(const std::vector<Channel *> joinedChannels, Client &client) 
-{
-    std::cout << "Channels I've joined:" << std::endl;
-    for(unsigned int i = 0; i < joinedChannels.size(); i++) 
-    {
-        client.send_message(joinedChannels[i]->getName());
-        client.send_message("\n");
-    }
-}
-
-
-void funFactCommand(Client &client) 
-{
-    client.send_message("Random fact: The first computer bug was an actual bug.");
-}
-
-void IRCserv::handleBot(char *msg, Client &client)
-{
-    char *cmd = strtok(msg, " ");
-    if (!cmd || strcmp(cmd, "BOT"))
-        return;
-    char *chcmd = strtok(NULL, "");
-    if (!chcmd)
-    {
-        client.send_message(ERR_NEEDMOREPARAMS(client.nick, this->getHostName()));
-        return;
-    }
-    std::string command(chcmd);
-    if (command == "/help") {
-        helpCommand(client);
-    } else if (command == "/time") {
-        timeCommand(client);
-    } else if (command == "/channels") {
-        channelsCommand(client.getChannels(), client);
-    } else if (command == "/funFact") {
-        funFactCommand(client);
-    } else {
-        std::cout << "Unknown command: " << command << std::endl;
-    }
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    if (bind(fd, (const sockaddr *)&serverAddress, sizeof(sockaddr)) < 0)
+        throw std::runtime_error('":bind() fail!');
 
 }
-
-
